11078a8f2ac79ecabc8bfb510b56e1f05e6bc42c
Ryan Breidenbach <ryan.breidenbach@invitationhomes.com>
2021-11-03 15:55:01 -0500

    ENG-80 Use production deployments API endpoint
    All of the CI/CD functionality has been deployed to production, so we
    should start using it there.

--- src/index.js
+++ src/index.js
@@ -133,7 +133,7 @@ async function postDeploymentDetails(cloudhub_env, cloudhub_app_name, is_success
 	try {		
 		const response = await axios({
             method: "post",
-            url: `https://api-dev.invitationhomes.com/ci-cd/v1/deployments`,
+            url: `https://api.invitationhomes.com/ci-cd/v1/deployments`,
             headers: {
                 'Authorization': `Bearer ${process.env.CI_CD_API_TOKEN}`
             },
